e81bf40f2f1b26709386c27cbad6dc700e522c9a
BlackSoulHub <mr.asaglaev@mail.ru>
2025-09-19 07:38:48 +0300

    rename useAsyncEffect -> useEffectAsync

--- src/shared/hooks/useEffectAsync.ts
+++ src/shared/hooks/useEffectAsync.ts
@@ -1,8 +1,8 @@
 import React, { useEffect } from "react"
 
-export const useAsyncEffect = (effect: () => Promise<void>, deps: React.DependencyList) => {
+export const useEffetcAsync = (effect: () => Promise<void>, deps: React.DependencyList) => {
     useEffect(() => {
-        effect().then(() => {})
+        effect().then(() => { })
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, deps)
 }
