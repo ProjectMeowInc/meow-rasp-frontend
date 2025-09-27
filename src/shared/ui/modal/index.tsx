84d349161efd6338fe915102242c9dcc9341dbce
BlackSoulHub <mr.asaglaev@mail.ru>
2025-09-27 17:30:50 +0300

    use modal wrapper in modal

--- src/shared/ui/modal/index.tsx
+++ src/shared/ui/modal/index.tsx
@@ -1,6 +1,6 @@
 import React from "react"
-import styles from "./styles.module.css"
 import ModalButtons from "./ui/modal-buttons"
+import ModalWrapper from "../modal-wrapper"
 
 export { default as ModalLabel } from "./ui/modal-label"
 export { default as ModalButtons } from "./ui/modal-buttons"
@@ -19,27 +19,12 @@ const Modal: React.FC<IModalProps> = ({ title, isOpen, onSubmit, onClose, childr
     }
 
     return (
-        <div className={styles.overlay} onClick={onClose}>
-            <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
-                <div className={styles.header}>
-                    <h2>{title}</h2>
-                    <button className={styles.close} onClick={onClose}>
-                        ×
-                    </button>
-                </div>
-                <div className={styles.body}>
-                    <>{children}</>
-                    <>
-                        <ModalButtons
-                            submitVariant="success"
-                            cancelVariant="danger"
-                            onSubmit={onSubmit}
-                            onCancel={onClose}
-                        />
-                    </>
-                </div>
-            </div>
-        </div>
+        <ModalWrapper title={title} onClose={onClose}>
+            <>{children}</>
+            <>
+                <ModalButtons submitVariant="success" cancelVariant="danger" onSubmit={onSubmit} onCancel={onClose} />
+            </>
+        </ModalWrapper>
     )
 }
 
